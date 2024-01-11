03814b4c860c7b8ff41ed0518a4e628457d3ce78
spicadev <155805184+spicadev@users.noreply.github.com>
2024-01-11 15:59:40 +0700

    Update types.js

--- js/types.js
+++ js/types.js
@@ -18,7 +18,11 @@ if(wh.ok) {
       if(!(entity instanceof wh.Entity)) {
         wh.error('Failed to set (' + x + ', ' + y + ') to', entity)
         return false
-      } else if(entity.world != this) return entity.world = this
+      } else if(entity.world != this) {
+        wh.log(entity, 'world is not', this, ' changing!')
+        entity.world = this
+        return false
+      }
       this.entities[this.getEntityIndex(x, y)] = entity
       return true
     }
